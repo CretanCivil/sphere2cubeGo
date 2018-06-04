80c144cec2dacba602cc495276ac4603413421e8
leapar <leapar@163.com>
2018-06-04 13:45:34 +0800

    e

--- saver/saver.go
+++ saver/saver.go
@@ -49,10 +49,19 @@ func SaveTile(tileResult worker.TileResult, outPutDir string) error {
 		}
 	}
 
+	src2k := imaging.Resize(tileResult.Image, 2048, 0, imaging.Linear)
 	for  i := 0; i < 4;i++ {
 		for  j := 0; j < 4;j++ {
-			src512 = imaging.Crop(tileResult.Image,image.Rectangle{Min:image.Point{i*512,j*512},Max:image.Point{(i+1)*512,(j+1)*512}})
+			src512 = imaging.Crop(src2k,image.Rectangle{Min:image.Point{i*512,j*512},Max:image.Point{(i+1)*512,(j+1)*512}})
 			err = imaging.Save(src512, filepath.Join(outPutDir, fmt.Sprintf("tiles/2k_face"+tileResult.Tile.TileName+"_%d_%d.jpg",i,j)))
+		}
+	}
+
+	src4k := imaging.Resize(tileResult.Image, 4096, 0, imaging.Linear)
+	for  i := 0; i < 8;i++ {
+		for  j := 0; j < 8;j++ {
+			src512 = imaging.Crop(src4k,image.Rectangle{Min:image.Point{i*512,j*512},Max:image.Point{(i+1)*512,(j+1)*512}})
+			err = imaging.Save(src512, filepath.Join(outPutDir, fmt.Sprintf("tiles/4k_face"+tileResult.Tile.TileName+"_%d_%d.jpg",i,j)))
 		}
 	}
 
