5321ed2b687a7a1c7c196cfc8b5f76707ae42620
leapar <leapar@163.com>
2018-06-29 18:48:52 +0800

    krpano

--- main.go
+++ main.go
@@ -8,6 +8,13 @@ import (
 	"./saver"
 	"./worker"
 	"time"
+	"os/exec"
+	"bytes"
+	"syscall"
+	"github.com/jimuyida/glog"
+	"fmt"
+	"strings"
+	"path/filepath"
 )
 
 var (
@@ -20,13 +27,18 @@ var (
 		worker.TileLeft,
 	}
 
-	tileSize          = 2048
+	tileSize          = 4096
 	originalImagePath = ""
 	outPutDir         = "./build"
 
 	tileSizeCmd          = flag.Int("s", tileSize, "Size in px of final tile")
 	originalImagePathCmd = flag.String("i", "", "Path to input equirectangular panorama")
 	outPutDirCmd         = flag.String("o", outPutDir, "Path to output directory")
+
+	krpanoDir = "C:/Users/WXH/Downloads/krpa_32449/krpa/krpano-1.19-pr13/"
+	krpanoExe = krpanoDir+"krpanotools64.exe"
+	krpanoArgs = "-config="+krpanoDir+"templates/convertdroplets.config"
+	useKrPano = true
 )
 
 func main() {
@@ -49,27 +61,137 @@ func main() {
 		}
 	}
 
-	done := make(chan worker.TileResult)
 	timeStart := time.Now()
-	cacheResult := cache.CacheAnglesHandler(tileSize)
-	for _, tileName := range tileNames {
-		tile := worker.Tile{TileName: tileName, TileSize: tileSize}
-		go worker.Worker(tile, cacheResult, originalImagePath, done)
-	}
 
-	for range tileNames {
-		tileResult := <-done
-		err = saver.SaveTile(tileResult, outPutDir)
+	if useKrPano {
+		tilesDir := filepath.Dir(originalImagePath)
+		filename := filepath.Base(originalImagePath)
+		ext := filepath.Ext(originalImagePath)
 
-		if err != nil {
-			log.Fatal(err.Error())
-			os.Exit(2)
+
+		shortName := filename[0:len(filename)-len(ext)]
+		fmt.Println(tilesDir,filename,ext,shortName)
+
+		tiles := map[string]string{
+			"u" : "0",
+			"l" : "4",
+			"f" : "1",
+			"r": "2",
+			"b": "3",
+			"d": "5",
 		}
 
-		log.Printf("Process for tile %v --> finished", tileResult.Tile.TileName)
+
+		err,b,c := cutByKrpano(originalImagePath)
+		fmt.Println(err,b,c)
+		if err != nil {
+			return
+		}
+
+		for key, _ := range tiles {
+			tileFile := filepath.Join(tilesDir,shortName+"_"+key+".jpg")
+			fmt.Println(tileFile)
+			finfo, err := os.Stat(tileFile)
+			if err != nil || finfo.Size() <= 0{
+				return
+			}
+		}
+
+		for key, value := range tiles {
+			tileFile := filepath.Join(tilesDir, shortName+"_"+key+".jpg")
+			err = saver.SaveTileFile(tileFile,value, outPutDir)
+			log.Printf("Process for tile %v --> finished", value)
+		}
+
+	} else {
+		done := make(chan worker.TileResult)
+
+		cacheResult := cache.CacheAnglesHandler(tileSize)
+		for _, tileName := range tileNames {
+			tile := worker.Tile{TileName: tileName, TileSize: tileSize}
+			go worker.Worker(tile, cacheResult, originalImagePath, done)
+		}
+
+		for range tileNames {
+			tileResult := <-done
+			err = saver.SaveTile(tileResult, outPutDir)
+
+			if err != nil {
+				log.Fatal(err.Error())
+				os.Exit(2)
+			}
+
+			log.Printf("Process for tile %v --> finished", tileResult.Tile.TileName)
+		}
 	}
+
 
 	timeFinish := time.Now()
 	duration := timeFinish.Sub(timeStart)
 	log.Printf("Time to render: %v seconds", duration.Seconds())
+}
+
+func cmdRunWithTimeout(cmd *exec.Cmd, timeout time.Duration) (error, bool) {
+	done := make(chan error)
+	go func() {
+		done <- cmd.Wait()
+	}()
+
+	var err error
+	select {
+	case <-time.After(timeout):
+		// timeout
+		if err = cmd.Process.Kill(); err != nil {
+			glog.Error("failed to kill: %s, error: %s", cmd.Path, err)
+		}
+		go func() {
+			<-done // allow goroutine to exit
+		}()
+		glog.Info("process:%s killed", cmd.Path)
+		return err, true
+	case err = <-done:
+		return err, false
+	}
+}
+
+func cutByKrpano(filepath string) (err error, exitCode int, outStr string) {
+	filepath = strings.Replace(filepath,"\\","/",-1)
+//	fmt.Println(krpanoExe,"sphere2cube "+filepath + krpanoArgs)
+
+
+	cmd := exec.Command(krpanoExe, "sphere2cube",filepath,"-q",  krpanoArgs)
+	var out bytes.Buffer
+	cmd.Stdout = &out
+	if err := cmd.Start(); err != nil {
+		return err,-1,err.Error()
+	}
+
+	var isTimeout bool
+	err,isTimeout = cmdRunWithTimeout(cmd,time.Duration(5*time.Minute))
+	if isTimeout {
+		exitCode = -1
+		return nil, exitCode, "time out"
+	}
+	if err != nil {
+		//glog.Error(err)
+		if exitError, ok := err.(*exec.ExitError); ok {
+			ws := exitError.Sys().(syscall.WaitStatus)
+			exitCode = ws.ExitStatus()
+		} else {
+			// This will happen (in OSX) if `name` is not available in $PATH,
+			// in this situation, exit code could not be get, and stderr will be
+			// empty string very likely, so we use the default fail code, and format err
+			// to string and set to stderr
+			//log.Printf("Could not get exit code for failed program: %v, %v", name, args)
+			exitCode = -1
+		}
+		outStr = out.String()
+		return err, exitCode, outStr
+	} else {
+		ws := cmd.ProcessState.Sys().(syscall.WaitStatus)
+		exitCode = ws.ExitStatus()
+	}
+
+	outStr = out.String()
+	return nil, exitCode, outStr
 }
--- saver/saver.go
+++ saver/saver.go
@@ -67,3 +67,50 @@ func SaveTile(tileResult worker.TileResult, outPutDir string) error {
 
 	return nil
 }
+
+func SaveTileFile(tileFile ,tileName, outPutDir string) error {
+
+	err := os.Mkdir(outPutDir, os.FileMode(os.ModePerm))
+
+	if err != nil {
+		if !os.IsExist(err) {
+			return err
+		}
+	}
+
+	err = os.Mkdir(outPutDir+"/tiles", os.FileMode(os.ModePerm))
+
+	tileImage,err := imaging.Open(tileFile)
+	if err != nil {
+		return err
+	}
+
+	src512 := imaging.Resize(tileImage, 512, 0, imaging.Linear)
+	err = imaging.Save(src512, filepath.Join(outPutDir, "tiles/512_face"+tileName+"_0_0.jpg"))
+
+	src51k := imaging.Resize(tileImage, 1024, 0, imaging.Linear)
+	for  i := 0; i < 2;i++ {
+		for  j := 0; j < 2;j++ {
+			src512 = imaging.Crop(src51k,image.Rectangle{Min:image.Point{i*512,j*512},Max:image.Point{(i+1)*512,(j+1)*512}})
+			err = imaging.Save(src512, filepath.Join(outPutDir, fmt.Sprintf("tiles/1k_face"+tileName+"_%d_%d.jpg",i,j)))
+		}
+	}
+
+	src2k := imaging.Resize(tileImage, 2048, 0, imaging.Linear)
+	for  i := 0; i < 4;i++ {
+		for  j := 0; j < 4;j++ {
+			src512 = imaging.Crop(src2k,image.Rectangle{Min:image.Point{i*512,j*512},Max:image.Point{(i+1)*512,(j+1)*512}})
+			err = imaging.Save(src512, filepath.Join(outPutDir, fmt.Sprintf("tiles/2k_face"+tileName+"_%d_%d.jpg",i,j)))
+		}
+	}
+
+	src4k := imaging.Resize(tileImage, 4096, 0, imaging.Linear)
+	for  i := 0; i < 8;i++ {
+		for  j := 0; j < 8;j++ {
+			src512 = imaging.Crop(src4k,image.Rectangle{Min:image.Point{i*512,j*512},Max:image.Point{(i+1)*512,(j+1)*512}})
+			err = imaging.Save(src512, filepath.Join(outPutDir, fmt.Sprintf("tiles/4k_face"+tileName+"_%d_%d.jpg",i,j)))
+		}
+	}
+
+	return nil
+}
